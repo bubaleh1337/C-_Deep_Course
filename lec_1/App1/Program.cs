6700c70f8a67258950af66195599a5ab18f3fca9
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-12-20 12:30:29 +0500

    Initial commit

---
+++ lec_1/App1/Program.cs
@@ -0,0 +1,22 @@
+﻿// using System;
+
+namespace Lection1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string name = args[0];
+                string message = $"Привет, {name}";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
+
