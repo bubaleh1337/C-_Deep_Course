320057e3e3fab37c04eb26579b95a78d5f9405c7
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-12-20 13:03:43 +0500

    added solution for hw1 of sem1

---
+++ sem_1/hw1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            int a = getNumber("Input first number --> ");
+            int b = getNumber("Input second number --> ");
+            string sign = getChar("Input +, -, * or / --> ");
+            mathOperation(a, b, sign);
+        }
+    }
+
+    static int getNumber(string msg)
+    {
+        Console.Write(msg);
+        int number = int.Parse(System.Console.ReadLine()!);
+        return number;
+    }
+
+    static string getChar(string msg)
+    {
+        System.Console.Write(msg);
+        string symbol = System.Console.ReadLine()!;
+        return symbol;
+    }
+
+    static void mathOperation(int a, int b, string sign)
+    {
+        switch (sign)
+        {
+            case "+":
+                System.Console.WriteLine($"{a} + {b} = {a + b}");
+                break;
+            case "-":
+                System.Console.WriteLine($"{a} - {b} = {a - b}");
+                break;
+            case "*":
+                System.Console.WriteLine($"{a} * {b} = {a * b}");
+                break;
+            case "/":
+                System.Console.WriteLine($"{a} / {b} = {a / b}");
+                break;
+            default:
+                System.Console.WriteLine("Try again, use only +, -, *, /");
+                break;
+
+        }
+    }
+}
